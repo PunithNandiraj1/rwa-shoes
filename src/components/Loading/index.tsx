228257b6f3ba99fe2d1ca46e4c78d1268f073e5b
Punith Nandiraj <punithnandiraj@gmail.com>
2023-12-09 19:13:09 +0530

    Loading Component Completed

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Loading: FC = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="border-4 border-transparent border-t-4 border-t-blue-600 rounded-full w-4 h-4 animate-spin"></div>
+        </div>
+    );
+};
